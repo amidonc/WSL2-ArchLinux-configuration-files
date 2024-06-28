67a481f35addbaf5e8b31f64591315e6b8efe053
amidonc <25570509+amidonc@users.noreply.github.com>
2024-06-28 12:16:45 -0400

    Update and rename hyprland.py to desktop profiles/hyprland.py

--- desktop profiles/hyprland.py
+++ desktop profiles/hyprland.py
@@ -26,9 +26,9 @@ class HyprlandProfile(XorgProfile):
 		return [
 			"hyprland",
 			"dunst",
-			"kitty",
+			"foot",
 			"dolphin",
-			"wofi",
+			"rofi-wayland",
 			"xdg-desktop-portal-hyprland",
 			"qt5-wayland",
 			"qt6-wayland",
